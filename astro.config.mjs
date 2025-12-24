8d46cf8d11390310fb4c447e2a6b0db484d2a99a
leisureea1 <486261950@qq.com>
2025-12-24 22:05:02 +0800

    cesh

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://blog.leisureea.cn/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
